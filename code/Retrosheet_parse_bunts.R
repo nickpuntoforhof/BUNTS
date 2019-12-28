f436c924e2a596453b9b80ddc3cbd06007cba769
nickpuntoforhof <nickpuntoforhof@gmail.com>
2019-12-28 13:16:37 -0600

    Initial Script Creation

---
+++ code/Retrosheet_parse_bunts.R
@@ -0,0 +1,8 @@
+### Retrosheet_parse_bunts.R ###
+### Noah Stafford ###
+### This file parses retroshseet data and ###
+### finds all plate appearances that ended with a bunt ###
+
+### Set paths
+input_dir <- "C:/Users/woody/OneDrive/Documents/retrosheet/"
+output_dir <- "C:/Users/woody/OneDrive/Documents/githubrepos/BUNTS/output/"
